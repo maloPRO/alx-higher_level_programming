91f739bbae1f1d60ff64c420f839747024b922fd
malopro <malovagilbert@gmail.com>
2023-05-11 07:16:04 -0700

    Task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,7 @@ import sys
 
 def args():
     if len(sys.argv) == 1:
-        print("{} arguments".format(len(sys.argv) - 1))
+        print("{} arguments.".format(len(sys.argv) - 1))
     elif len(sys.argv) == 2:
         print("{} argument:".format(len(sys.argv) - 1))
         for i in range(len(sys.argv) - 1):
