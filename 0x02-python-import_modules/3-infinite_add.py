7a4e040031890c9f3196eef39d34527344ba22d7
malopro <malovagilbert@gmail.com>
2023-05-11 10:55:04 -0700

    Task 3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+
+def add():
+    sum = 0
+    for i in range(len(sys.argv) - 1):
+        sum += int(sys.argv[i + 1])
+    print(f"{sum}")
+
+
+if __name__ == "__main__":
+    add()
