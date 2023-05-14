3a02f38684c00b614635ce7ca9d79f43d02b213d
malopro <malovagilbert@gmail.com>
2023-05-14 03:55:59 -0700

    Task 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
 def max_integer(my_list=[]):
-    max_int = 0
-
     if not my_list:
         return None
     else:
+        max_int = 0
+
         for i in my_list:
             if i > max_int:
                 max_int = i
