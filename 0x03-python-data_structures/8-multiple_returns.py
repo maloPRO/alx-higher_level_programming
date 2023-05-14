ac776f9b82e174632fbb0bf102d3099339882aea
malopro <malovagilbert@gmail.com>
2023-05-14 10:02:23 -0700

    Task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    if sentence:
-        new_tuple = (len(sentence), sentence[0].upper())
-    else:
+    if not sentence:
         return None
-    return new_tuple
+    return (len(sentence), sentence[0].upper())
