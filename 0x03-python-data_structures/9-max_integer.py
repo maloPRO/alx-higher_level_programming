896fe768b32b74269ff268a987f0c34e1444976e
malopro <malovagilbert@gmail.com>
2023-05-15 11:30:07 -0700

    Task 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,9 +3,10 @@
 def max_integer(my_list=[]):
     max_value = 0
 
-    if not my_list:
+    if len(my_list) > 0:
+        for i in my_list:
+            if i > max_value:
+                max_value = i
+    else:
         return None
-    for i in my_list:
-        if i > max_value:
-            max_value = i
     return max_value
