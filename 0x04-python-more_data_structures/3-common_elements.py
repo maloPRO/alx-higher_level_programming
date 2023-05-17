6976c569efd54a13e13d2056966c4c8f68c11b6d
Gilbert Malova <malovagilbert@gmail.com>
2023-05-17 19:37:59 +0200

    Task 3

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
 def common_elements(set_1, set_2):
-    for item in set_1:
-        if item in set_2:
-            return item
+    common = filter(lambda item: item in set_2, set_1)
+    return next(common, None)
