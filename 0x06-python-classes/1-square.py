df003f7bb113046e30bb7198f78feb72f9ef7909
malopro <malovagilbert@gmail.com>
2023-05-30 10:09:58 -0700

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+""" Defines a square """
+
+class Square:
+    """ Creates a square. """
+
+    def __init__ (self, size):
+        """
+        Initializes s instance of square.
+
+        Args:
+            size: size of square.
+        """
+        self.__size = size
