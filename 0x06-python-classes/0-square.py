ef7e653987271f46c5759949cad21c3b96a3f7d1
malopro <malovagilbert@gmail.com>
2023-05-30 09:42:40 -0700

    Task 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -2,9 +2,12 @@
 
 class Square:
     """
-    Defines a square
+    Defines a square.
+
+    Allows user to create squares.
 
     Attributes:
-        length (int): length of sides
+        length (int): length of sides.
+
     """
     pass
