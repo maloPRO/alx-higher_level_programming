b5b3446e86ef34aed38e5957362f083adbd49874
maloPRO <malovagilbert@gmail.com>
2023-05-31 02:40:13 +0200

    Task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -78,8 +78,8 @@ class Square:
         """ Prints square using # """
         if self.__size == 0:
             print()
-        else:
-            for i in range(self.__size):
-                for j in range(self.__size):
-                    print("#".format(), end="")
-                print()
+        for _ in range(self.__position[1]):
+            print()
+
+        for _ in range(self.__size):
+            print(" " * self.__position[0] + "#" * self.__size)
