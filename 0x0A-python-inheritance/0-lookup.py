9e0af54b30a059c128cdd0724a519debd76142c5
malopro <malovagilbert@gmail.com>
2023-06-12 12:54:46 -0700

    Task 0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -3,4 +3,16 @@
 """ Gets objects of a class """
 
 def lookup(obj):
+    """
+    Returns methods and attributes
+
+    Args:
+        obj (object): object 
+
+    Returns:
+        list: list of methods and attributes
+    
+    Raises:
+        TypeError: If obj is not a class
+    """
     return dir(obj)
