d7eb421032dd64c594a6200da17fba5c9c78a8e5
malopro <malovagilbert@gmail.com>
2023-06-12 15:25:51 -0700

    Task 7

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -30,10 +30,7 @@ class BaseGeometry:
 
         """
 
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
-            return False
         elif value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
-            return False
-        return True
