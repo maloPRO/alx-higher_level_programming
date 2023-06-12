03cb2007ce52d690f92e5507ee588b725f8f4fdf
malopro <malovagilbert@gmail.com>
2023-06-12 14:38:21 -0700

    Task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+""" Geometry class """
+
+
+class BaseGeometry:
+    """ Does geometry """
+    
+    def area(self):
+        """ raises error """
+
+        raise Exception("area() is not implemented")
