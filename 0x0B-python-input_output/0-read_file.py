710eabc15f21cd94745604e9c5cdf6078e940a99
malopro <malovagilbert@gmail.com>
2023-06-13 03:13:02 -0700

    Task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+""" Opens file """
+
+
+def read_file(filename=""):
+    """
+    Opens and reads a file
+
+    Args:
+        filename (str): Filename
+    """
+
+    with open(filename, encoding='utf-8') as f:
+        print(f.read())
