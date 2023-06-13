b8c479b80753f7c50fb575ecdd6f014dc212b4cc
malopro <malovagilbert@gmail.com>
2023-06-13 11:22:11 -0700

    Task 3

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
 
+""" 
+This module contains a function
+that converts an object  to json string
+"""
 import json
-
-""" serialize to json string """
-
 
 def to_json_string(my_obj):
     """
-    Converts string to JSON
+    Converts object  to JSON
 
     Args:
-        my_obj (str): string to be converted to json
+        my_obj: string to be converted to json
+
+    Returns:
+        str: JSON rep of object
     """
 
     return json.dumps(my_obj, sort_keys=True)
