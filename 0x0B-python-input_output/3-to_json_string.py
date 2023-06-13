89b94b11e1fd429e50492cde9547431b00c32c27
malopro <malovagilbert@gmail.com>
2023-06-13 06:45:34 -0700

    Task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import json
+
+""" serialize to json string """
+
+
+def to_json_string(my_obj):
+    """
+    Converts string to JSON
+
+    Args:
+        my_obj (str): string to be converted to json
+    """
+
+    return json.dumps(my_obj)
