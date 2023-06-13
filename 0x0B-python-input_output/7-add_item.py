d6b0ee011c00923e87ddb242ac652545276e0ad7
malopro <malovagilbert@gmail.com>
2023-06-13 13:34:42 -0700

    Task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,12 +6,16 @@ This script adds all arguments to a Python list
 
 import sys
 import json
+import os
 
 save_to_json = __import__('5-save_to_json_file').save_to_json_file
 load_from_json = __import__('6-load_from_json_file').load_from_json_file
 
 args = sys.argv[1:]
-argList = load_from_json('add_item.json')
+if os.path.exists('add_item.json'):
+    argList = list(load_from_json('add_item.json'))
+else:
+    argList = []
 
 argList += args
 
