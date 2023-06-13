200558b1d62fb111b6f373dd5c8ccf5a0463d2b8
malopro <malovagilbert@gmail.com>
2023-06-13 11:28:16 -0700

    Task 3

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 """
-This module contains a function
-that converts an object  to json string
+This module contains a function that returns
+the JSON representation of an object (string)
 """
 
 import json
