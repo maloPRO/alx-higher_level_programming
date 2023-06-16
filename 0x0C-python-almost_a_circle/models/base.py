9c8ab51a925cfc7237c77c3da4f01ea151d766b1
maloPRO <malovagilbert@gmail.com>
2023-06-16 05:31:19 -0700

    Task 1

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+""" This module contains the Base class """
+
+class Base:
+    """ Base class """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """ initializes Base class """
+
+        if id is not None:
+            self.id = id
+        else: 
+            Base.__nb_objects += 1
+            self.id = Base. __nb_objects
