7c387d933d407b4c8e50987f2336a801ec3a5978
maloPRO <malovagilbert@gmail.com>
2023-06-20 03:12:15 -0700

    Task

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -11,3 +11,14 @@ class TestBase(unittest.TestCase):
     def test_positive_id(self):
         my_obj = Base(10)
         self.assertEqual(my_obj.id, 10)
+
+    def test_id_none(self):
+        b1 = Base()
+        b2 = Base()
+        b3 = Base()
+
+        self.assertEqual(b3.id, 3)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+
+""" Unittest for rectangle class """
+
+import unittest
+from models.rectangle import Rectangle
+
+class TestRectangle(unittest.TestCase):
+    """ Rectangle class unittest """
+
+    def test_id(self):
+        r = Rectangle(10, 9, 0, 0, 11)
+        self.assertEqual(r.id, 11)
+
+    def test_sides(self):
+        r = Rectangle(10, 8, 4, 2)
+        self.assertEqual(r.width, 10)
+        self.assertEqual(r.height, 8)
+        self.assertEqual(r.x, 4)
+        self.assertEqual(r.y, 2)
+
+    def test_exceptions(self):
+        with self.assertRaises(TypeError):
+            r = Rectangle('one', 10)
+
+        with self.assertRaises(TypeError):
+            r = Rectangle(3, 'one')
+
+        with self.assertRaises(TypeError):
+            r = Rectangle(3, 4, 'one', 3)
+
+        with self.assertRaises(TypeError):
+            r = Rectangle(3, 5, 1, 'one')
+
+        with self.assertRaises(ValueError):
+            r = Rectangle(0, 4)
+
+        with self.assertRaises(ValueError):
+            r = Rectangle(-3, 4)
+
+        with self.assertRaises(ValueError):
+            r = Rectangle(3, 0)
+
+        with self.assertRaises(ValueError):
+            r = Rectangle(3, -9)
+
+        with self.assertRaises(ValueError):
+            r = Rectangle(3, 5, -3, 4)
+
+        with self.assertRaises(ValueError):
+            r = Rectangle(3, 4, 4, -8)
+
+    def test_area(self):
+        r = Rectangle(4, 10)
+        self.assertEqual(r.area(), 40)
+
+    def test_display(self):
+        r = Rectangle(2, 2)
+        self.assertEqual(r.display(), "##\n##\n")
