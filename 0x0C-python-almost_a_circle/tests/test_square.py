f9144bb925756ca4bd6e37fcaee618f67e9a6f7d
maloPRO <malovagilbert@gmail.com>
2023-06-22 04:14:06 -0700

    Task 10 test

---
+++ 0x0C-python-almost_a_circle/tests/test_square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+""" Unit tests for squarepy """
+
+import unittest
+from models.square import Square
+
+class TestSquare(unittest.TestCase):
+
+    def test_size(self):
+        s = Square(5)
+        self.assertEqual(s.width, 5)
