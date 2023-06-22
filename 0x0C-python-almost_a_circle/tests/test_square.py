82ec12b932bbae936aad8aa050ef8bd36ca3d149
maloPRO <malovagilbert@gmail.com>
2023-06-22 04:16:21 -0700

    Task 10 test

--- 0x0C-python-almost_a_circle/tests/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_square.py
@@ -10,3 +10,7 @@ class TestSquare(unittest.TestCase):
     def test_size(self):
         s = Square(5)
         self.assertEqual(s.width, 5)
+
+    def test_area(self):
+        s = Square(5)
+        self.assertEqual(s.area(), 25)
