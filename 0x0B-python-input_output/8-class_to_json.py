d6f79e0b371be08c2ab20e74d8e15cafbd1ec6c5
malopro <malovagilbert@gmail.com>
2023-06-23 23:25:38 -0700

    task 8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -12,15 +12,4 @@ def class_to_json(obj):
     """
     Gets dict desc
     """
-    if isinstance(obj, (list, tuple)):
-        return [class_to_json(item) for item in obj]
-    elif isinstance(obj, dict):
-        return {class_to_json(key): class_to_json(value) for key, value in obj.items()}
-    elif isinstance(obj, str):
-        return obj
-    elif isinstance(obj, (int, bool)):
-        return obj
-    elif hasattr(obj, '__dict__'):
-        return class_to_json(obj.__dict__)
-    else:
-        return None
+    return obj.__dict__
