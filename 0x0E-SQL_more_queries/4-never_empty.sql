1163b27d899c5d14b4c0c5d33aa01925cb97db02
malopro <malovagilbert@gmail.com>
2023-07-19 03:45:52 -0700

    Task 4

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,7 +1,6 @@
 -- Creates table
 -- id cant be NULL
-DROP TABLE IF EXISTS id_not_null;
-CREATE TABLE id_not_null(
+CREATE TABLE IF NOT EXISTS id_not_null(
 	id INT DEFAULT 1,
 	name VARCHAR(256)
 );
