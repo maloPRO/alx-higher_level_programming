55d32c280f9f91dd4d8077f76273ca1f99a38b92
malopro <malovagilbert@gmail.com>
2023-07-19 11:11:09 -0700

    removed \

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,6 @@
+-- Lists shows where genre is not linked
+SELECT tv_shows.title, tv_show_genres.genre_id AS genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE genre_id = NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
