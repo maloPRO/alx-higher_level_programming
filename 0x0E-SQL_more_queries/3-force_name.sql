708f32494a50b8b7e3f62d83abc6b5b2469e4084
malopro <malovagilbert@gmail.com>
2023-07-19 03:43:22 -0700

    Task 3

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,7 +1,6 @@
 -- Creates a table
 -- name cant be NULL
-DROP TABLE IF EXISTS force_name;
-CREATE TABLE force_name(
+CREATE TABLEi IF NOT EXISTS force_name(
 	id INT,
 	name VARCHAR(256) NOT NULL
 );
