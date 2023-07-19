d607a0c651b97df5aba40c0c4076b938cd96d742
malopro <malovagilbert@gmail.com>
2023-07-19 04:06:15 -0700

    Task 7

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,9 @@
+-- Creates daatabase hbtn_0d_usa and table cities
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities(
+	id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	state_id INT NOT NULL FOREIGN KEYid) REFERENCES states(id),
+	name VARCHAR(256)  NOT NULL
+	)
+	type=InnoDB;
