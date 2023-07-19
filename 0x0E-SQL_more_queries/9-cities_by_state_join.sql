31f74f63ec8fd82a57eaed2098322028f1ab1744
malopro <malovagilbert@gmail.com>
2023-07-19 07:06:30 -0700

    Task 9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- Lists all cities
 SELECT cities.id, cities.name, states.name
 FROM cities
-	INNER JOIN states ON cities.state_id = state.id
+INNER JOIN states ON cities.state_id = states.id
 ORDER BY cities.id;
