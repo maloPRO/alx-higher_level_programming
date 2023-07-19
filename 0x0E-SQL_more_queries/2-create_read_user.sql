597c35e59f4a67e6d38648602d629002c96fa07a
malopro <malovagilbert@gmail.com>
2023-07-19 05:08:46 -0700

    Task 2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -3,4 +3,4 @@ DROP DATABASE IF EXISTS hbtn_0d_2;
 CREATE DATABASE hbtn_0d_2;
 DROP USER IF EXISTS user_0d_2;
 CREATE USER 'user_0d_2'@'localhost';
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2 TO 'user_0d_2'@'localhost';
