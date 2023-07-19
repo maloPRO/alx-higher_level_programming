7535bac657149527de07bd686b55874c129a8413
malopro <malovagilbert@gmail.com>
2023-07-19 12:13:10 -0700

    Task 16

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- lists all shows, and all genres linked to that show
 SELECT tv_shows.title AS title, IFNULL(tv_genres.name, NULL) AS name
 FROM tv_shows
-JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
-JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 ORDER BY title, name;
