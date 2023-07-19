05ab959260ecc440548815d3eed1a2f6e5dcc9e5
malopro <malovagilbert@gmail.com>
2023-07-19 05:12:41 -0700

    Task 2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,4 @@
 -- Creates database hbtn_0d_2 and user user_0d_2
-DROP DATABASE IF EXISTS hbtn_0d_2;
-CREATE DATABASE hbtn_0d_2;
-DROP USER IF EXISTS user_0d_2;
-CREATE USER 'user_0d_2'@'localhost';
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
