9afc7bf186ff2fe327fd13e0d4e1303ab779022d
malopro <malovagilbert@gmail.com>
2023-08-14 02:41:11 -0700

    Task 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,6 +4,6 @@ for (let i = 0; i < process.argv[2]; i++) {
   if ((!process.argv[2]) || (process.argv[2] < 0)) {
     console.log('Missing number of occurrences');
   } else {
-      console.log('C is fun');
-      }
+    console.log('C is fun');
+  }
 }
