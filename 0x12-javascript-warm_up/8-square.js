7dd349e0cccd7aa7ffef7e3ee377f4261412ca0b
malopro <malovagilbert@gmail.com>
2023-08-14 10:38:02 -0700

    .

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+if (!process.argv[2] || isNaN(process.argv[2])) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < process.argv[2]; i++) {
+	  for (let j = 0; j < process.argv[2]; j++) {
+		  console.log('X');
+	  }
+  }
+}
---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,3 @@
+#!/usr/bin/node
+
+function add
