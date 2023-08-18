66eefccd11567b13decbdfacb089e63de1e09eea
malopro <malovagilbert@gmail.com>
2023-08-18 06:37:54 -0700

    Task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,7 +6,12 @@ Contains a script that lists all states
 import MySQLdb
 import sys
 
-mydb = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+mydb = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3])
 
 cur = mydb.cursor()
 
