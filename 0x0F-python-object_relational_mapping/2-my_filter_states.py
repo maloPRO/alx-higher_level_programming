fb54cf507d93a23aa659bc8a59883cc881299bc5
malopro <malovagilbert@gmail.com>
2023-08-18 06:58:18 -0700

    Task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,15 +1,25 @@
 #!/usr/bin/python3
+
+"""
+prints state based on usser input
+"""
 import MySQLdb
 import sys
 
-mydb = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+if __name__ == "__main__":
+    mydb = MySQLdb.connect(host='localhost',
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3])
 
-cur = mydb.cursor()
+    cur = mydb.cursor()
 
-cur.execute('SELECT * FROM states ORDER BY id')
+    query = 'SELECT * FROM states ORDER BY id'
+    cur.execute(query)
+    inpt = sys.argv[4]
+    states = cur.fetchall()
 
-states = cur.fetchall()
-
-for state in states:
-    if state[1] == sys.argv[4]:
-        print(state)
+    for state in states:
+        if state[1] == inpt:
+            print(state)
