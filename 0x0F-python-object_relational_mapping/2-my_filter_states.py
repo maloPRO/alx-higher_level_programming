a84782500782a184a27a62a8cad8953e38217606
malopro <malovagilbert@gmail.com>
2023-08-17 21:05:34 -0700

    Task 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+mydb = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+
+cur = mydb.cursor()
+
+cur.execute('SELECT * FROM states ORDER BY id')
+
+states = cur.fetchall()
+
+for state in states:
+    if state[1] == sys.argv[4]:
+        print(state)
