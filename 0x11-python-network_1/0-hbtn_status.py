10e7f6d949fb0b6c17113dc59cec54ad82852ef0
malopro <malovagilbert@gmail.com>
2023-09-04 09:57:54 -0700

    Task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,5 +7,12 @@ url = 'https://alx-intranet.hbtn.io/status'
 
 if __name__ == "__main__":
     with urllib.request.urlopen(url) as response:
-        body = response.read()
-    print(body.type())
+
+        response_content_bytes = response.read()
+
+        response_content_utf8 = response_content_bytes.decode('utf-8')
+
+        print("Body response:")
+        print(f"\t- type: {type(response_content_bytes)}")
+        print(f"\t- content: {response_content_bytes}")
+        print(f"\t- utf8 content: {response_content_utf8}")
