ba98a43f9e580277297f3d67e15e6d62324f84a6
malopro <malovagilbert@gmail.com>
2023-09-04 10:02:52 -0700

    Task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,18 +1,13 @@
 #!/usr/bin/python3
-# Write a Python script that fetches https://alx-intranet.hbtn.io/status
-
+"""Fetches https://alx-intranet.hbtn.io/status."""
 import urllib.request
 
-url = 'https://alx-intranet.hbtn.io/status'
 
 if __name__ == "__main__":
-    with urllib.request.urlopen(url) as response:
-
-        response_content_bytes = response.read()
-
-        response_content_utf8 = response_content_bytes.decode('utf-8')
-
+    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
+        body = response.read()
         print("Body response:")
-        print(f"\t- type: {type(response_content_bytes)}")
-        print(f"\t- content: {response_content_bytes}")
-        print(f"\t- utf8 content: {response_content_utf8}")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
