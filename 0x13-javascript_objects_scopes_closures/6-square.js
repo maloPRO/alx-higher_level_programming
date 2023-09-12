ad1f755792c53997adfb2b10fa5b6fce1330d02e
maloPRO <malovagilbert@gmail.com>
2023-09-12 20:55:37 +0200

    Task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 
-module.exports = class Square extends require("./5-square") {
-  charPrint(c) {
+module.exports = class Square extends require('./5-square') {
+  charPrint (c) {
     if (c === undefined) {
       this.print();
     } else {
       for (let i = 0; i < this.height; i++) {
         for (let j = 0; j < this.width; j++) {
-          process.stdout.write("C");
+          process.stdout.write('C');
         }
         console.log();
       }
