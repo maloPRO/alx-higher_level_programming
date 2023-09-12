6f6e2853ff3de7a55ad2a48c715e4ebea270ef28
maloPRO <malovagilbert@gmail.com>
2023-09-12 20:51:04 +0200

    Task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,24 +1,19 @@
 #!/usr/bin/node
 
-const Rectangle = require('./4-rectangle');
+const Rectangle = require("./4-rectangle");
 
 class Square extends Rectangle {
-  constructor (size) {
+  constructor(size) {
     super(size, size);
   }
 
-  charPrint (c) {
-    if (c) {
-      for (let i = 0; i < this.width; i++) {
-        for (let j = 0; j < this.width; j++) {
-          process.stdout.write('C');
-        }
-        console.log();
-      }
+  charPrint(c) {
+    if (c === undefined) {
+      this.print();
     } else {
-      for (let i = 0; i < this.width; i++) {
+      for (let i = 0; i < this.height; i++) {
         for (let j = 0; j < this.width; j++) {
-          process.stdout.write('X');
+          process.stdout.write("C");
         }
         console.log();
       }
