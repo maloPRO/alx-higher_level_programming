f8bc1ae69950d01c65e9e31662683d06e77b4512
malopro <malovagilbert@gmail.com>
2023-11-13 07:55:57 -0800

    Task 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
-const { argv } = require('node:process');
 
-if (!argv[2]) {
+if (!process.argv[2]) {
   console.log('No argument');
 } else {
-  console.log(argv[2]);
+  console.log(process.argv[2]);
 }
