a18c18771a573be6821477dd9a72847fe47ad38d
malopro <malovagilbert@gmail.com>
2023-11-13 07:15:26 -0800

    Task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,6 +3,8 @@ const { argv } = require('node:process');
 
 if (argv.length === 2) {
   console.log('No argument');
+} else if (argv.length === 3) {
+  console.log('Argument found');
 } else {
-  console.log(argv[2]);
+  console.log('Arguments found');
 }
