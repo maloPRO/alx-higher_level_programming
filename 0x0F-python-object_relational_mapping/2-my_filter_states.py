1f40dc8b4b1461eb4a1bd618a34fc3c7d2e9e48a
malopro <malovagilbert@gmail.com>
2023-11-17 10:23:24 -0800

    Task 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This module lists state based on user input
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+
+    cur = db.cursor()
+    cur.execute(f"SELECT * FROM states \
+                  WHERE name LIKE BINARY '{sys.argv[4]}%' \
+                  ORDER BY states.id")
+
+    states = cur.fetchall()
+    for state in states:
+        print(state)
