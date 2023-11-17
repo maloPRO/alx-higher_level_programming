0d87e49bc83ae8a37a8d2662d7b44c2e1a2d8ca2
malopro <malovagilbert@gmail.com>
2023-11-17 09:50:03 -0800

    Task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-
+"""
+This module lists all states
+"""
 import MySQLdb
 import sys
 
-usr, pwd, hst, prt, dbase = sys.argv[1], sys.argv[2], "localhost", 3306, sys.argv[3]
 if __name__ == "__main__":
-    db = MySQLdb.connect(host=hst,
-                         port=prt,
-                         user=usr,
-                         passwd=pwd,
-                         db=dbase)
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
 
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY states.id")
