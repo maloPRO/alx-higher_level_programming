e5e0b9d27c7bcf6a74cd9bc72eecf10257f6a142
malopro <malovagilbert@gmail.com>
2023-11-19 04:20:48 -0800

    Task 8

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
 
     state = session.query(State).order_by(State.id).first()
 
-    is state:
+    if state:
         print("{}: {}".format(state.id, state.name))
     else:
         print("Nothing\n")
